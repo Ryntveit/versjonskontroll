702db32afe09f38a4f1c405c3cf569d27da849e1
Ryntveit <148530160+Ryntveit@users.noreply.github.com>
2023-10-26 18:20:53 +0200

    oppgave 8

---
+++ oppgaver/oppgave8.py
@@ -0,0 +1,50 @@
+#%%
+
+def tall(teller):
+    while True:
+        antall = input(f'{teller}:')
+        if antall.isnumeric():
+            antall = int(antall)
+            return antall
+        else:
+            print('Du må skrive et tall')
+ 
+def plassering(biler, plasser):
+    spillere_fordelt = []
+    if spillere <= biler*plasser:
+        spillere_i_bil = spillere // biler
+       
+        for i in range(biler):
+            spillere_fordelt.append(spillere_i_bil)
+ 
+        if spillere % biler == 0:
+            return spillere_fordelt
+        else:
+            rest = spillere % biler
+            j = 0
+            while rest != 0:
+                spillere_fordelt[j] = spillere_fordelt[j] + 1
+                rest -= 1
+                j += 1
+            return spillere_fordelt
+       
+    else:
+        for i in range(biler):
+            spillere_fordelt.append(plasser)
+       
+        rest = spillere - biler*plasser
+        spillere_fordelt.append(rest)
+        return spillere_fordelt
+spillere = tall('skriv antall spillere')      
+biler = tall('Skriv antall biler')
+plasser = tall('Skriv antall plasser i hver bil')
+ 
+fordelte_spillere = plassering(biler, plasser)
+if len(fordelte_spillere) == biler:
+    for i in range(biler):
+        print(f'Bil {i + 1}, skal ha {fordelte_spillere[i]} spillere.')
+else:
+    for i in range(biler):
+        print(f'Bil {i + 1}, er det {fordelte_spillere[i]} spillere.')
+    print(f'{fordelte_spillere[-1]} spillere får ikke plass i bilene.')
+# %%
